f267d6e0fd39fd1c700c996f2333dfb5c026037b
Luc Weinbrecht <luc@weinbrecht.de>
2023-10-13 10:06:07 +0200

    started testing the optimize api client

---
+++ src/main/kotlin/de/luc/weinbrecht/optimzetoxes/optimizeexport/adapter/out/optimize/DataQueryException.kt
@@ -0,0 +1,3 @@
+package de.luc.weinbrecht.optimzetoxes.optimizeexport.adapter.out.optimize
+
+class DataQueryException(message: String) : RuntimeException(message)
--- src/main/kotlin/de/luc/weinbrecht/optimzetoxes/optimizeexport/adapter/out/optimize/OptimizeRawDataQueryService.kt
+++ src/main/kotlin/de/luc/weinbrecht/optimzetoxes/optimizeexport/adapter/out/optimize/OptimizeRawDataQueryService.kt
@@ -2,8 +2,10 @@ package de.luc.weinbrecht.optimzetoxes.optimizeexport.adapter.out.optimize
 
 import de.luc.weinbrecht.optimzetoxes.optimizeexport.domain.model.OptimizeData
 import de.luc.weinbrecht.optimzetoxes.optimizeexport.usecase.out.RawDataQuery
+import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Service
 class OptimizeRawDataQueryService(
@@ -11,12 +13,16 @@ class OptimizeRawDataQueryService(
     private val optimizeClientProperties: OptimizeClientProperties
 ): RawDataQuery {
 
+    @Throws(DataQueryException::class)
     override fun queryData(): OptimizeData = webClient
             .get()
             .uri("/api/public/export/report/${optimizeClientProperties.reportId}/result/json?paginationTimeout=60&limit=2")
             .retrieve()
+            .onStatus(HttpStatusCode::isError) { _ ->
+                Mono.error { throw DataQueryException("Could not fetch data from optimize") }
+            }
             .bodyToMono(OptimizeData::class.java)
             .blockOptional()
-            .orElseThrow { RuntimeException("Could not parse data from optimize") }
+            .orElseThrow { DataQueryException("Could not parse data from optimize") }
 
 }
