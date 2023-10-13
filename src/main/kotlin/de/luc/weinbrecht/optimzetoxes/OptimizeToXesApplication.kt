f5bd8aeecae802bcdb4275dfaa419d461d374c26
Luc Weinbrecht <luc@weinbrecht.de>
2023-10-13 08:53:51 +0200

    moved runner impl to spring boot application

--- src/main/kotlin/de/luc/weinbrecht/optimzetoxes/OptimizeToXesApplication.kt
+++ src/main/kotlin/de/luc/weinbrecht/optimzetoxes/OptimizeToXesApplication.kt
@@ -1,10 +1,21 @@
 package de.luc.weinbrecht.optimzetoxes
 
+import de.luc.weinbrecht.optimzetoxes.optimizeexport.adapter.out.optimize.OptimizeRawDataQueryService
+import de.luc.weinbrecht.optimzetoxes.xesmapping.toXes
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class OptimizeToXesApplication
+class OptimizeToXesApplication(
+	private val optimizeRawDataQueryService: OptimizeRawDataQueryService,
+): CommandLineRunner, Logging {
+	override fun run(vararg args: String?) {
+		val result = optimizeRawDataQueryService.queryData()
+		log().info("Fetch data {}", result)
+		log().info("Converting raw data to XES: {}", result.toXes())
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<OptimizeToXesApplication>(*args)
